f72669ee59eccf549002169dffd3749d21c61483
vladislavStel <stelmach.vladislav@yandex.ru>
2023-08-07 09:23:02 +0300

    fix: fix1

--- ewm-stats-service/common-dto/src/main/java/ru/practicum/ewm/commonDto/dto/EndpointHitDto.java
+++ ewm-stats-service/common-dto/src/main/java/ru/practicum/ewm/commonDto/dto/EndpointHitDto.java
@@ -3,6 +3,8 @@ package ru.practicum.ewm.commonDto.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
@@ -12,10 +14,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class EndpointHitDto {
 
+    @NotBlank
     private String app;
+    @NotBlank
     private String uri;
+    @NotBlank
     private String ip;
-
+    @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
     
